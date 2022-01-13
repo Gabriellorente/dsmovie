4d879a888c8d5f2b48a2654f88aed30c4a9a75f3
Gabriellorente <lorentegabriel35@gmail.com>
2022-01-13 13:17:48 -0300

    teste

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -13,7 +13,7 @@ function Navbar() {
                     <a href="https://github.com/Gabriellorente">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p>/devsuperior</p>
+                            <p className="dsmovie-contact-link">/devsuperior</p>
                         </div>
                     </a>
                 </div>
