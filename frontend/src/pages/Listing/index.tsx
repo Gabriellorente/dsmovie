ba15c64d2e792dd4cf2e03b9c4ac9b451f1216e9
Gabriellorente <lorentegabriel35@gmail.com>
2022-01-14 01:07:54 -0300

    fail

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,11 @@
 import Pagination from "components/Pagination";
-import MovieStars from "components/MovieStars";
+
 function Listing() {
 
     return (
         <>
             <Pagination />
-            <MovieStars />
+            
         </>
     );
 }
